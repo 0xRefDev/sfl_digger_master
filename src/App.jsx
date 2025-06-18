722d5457128151b5f18ae6c8e3a793f7809878e7
Andres Hernandez <107950480+REFH4CK@users.noreply.github.com>
2025-06-18 01:25:39 -0400

    fix pieces map

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ export function App() {
   };
 
   let digs = 25;
-  if (piezas.length > 0) {
+  if (piezas != '') {
     piezas.map((p) => {
       if (p.name === "Nothing" || p.name === "Possible") {
         digs
