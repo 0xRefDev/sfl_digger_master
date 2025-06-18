9169ae2525e557ba2e57077eef816afd0d513ec0
REFH4CK <refh4ck.dev@gmail.com>
2025-06-17 23:01:49 -0400

    improve arrow and add donate address

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import logo from "../assets/images/logo.png";
 import trophy from "../assets/images/trophy.png";
 import discord from "../assets/images/discord.png";
+import arrow from "../assets/images/arrow.png";
 
 export function Header() {
   return (
     <>
-      <header className="relative flex items-center justify-center flex-col p-4 gap-3 max-w-[800px] mx-auto">
+      <header className="relative flex items-center justify-center flex-col p-4 gap-3 max-w-[550px] mx-auto">
         <a
           href="https://discord.gg/MexBzYbxju"
           noreferrer
@@ -33,9 +34,17 @@ export function Header() {
           className="w-[15rem] h-auto select-none user-drag-none"
           style={{ WebkitUserDrag: "none" }}
         />
-        <h1 className="px-4 py-1 bg-[#030712]/60 rounded-full border-2 border-cyan-800/50">
+        <h1 className="relative px-4 py-1 bg-[#030712]/60 rounded-full border-2 border-cyan-800/50">
           Made with ❤️ by REFH4CK
+          <div className="absolute -z-10 flex justify-between w-[17.5rem] top-5 -translate-y-1/2 -left-7">
+            <span><img src={arrow} alt="Green pixelart arrow" className="size-6 -rotate-90" /></span>
+            <span><img src={arrow} alt="Green pixelart arrow" className="size-6 rotate-y-180 rotate-90" /></span>
+          </div>
+          <span className="absolute bg-amber-400/35 px-2 py-1 rounded-full -right-7 top-12 -translate-y-1/2 text-xs font-mono ">
+            0xAf57D68A12F28501580407B80B4d3690c9B74e62
+          </span>
         </h1>
+          
       </header>
     </>
   );
