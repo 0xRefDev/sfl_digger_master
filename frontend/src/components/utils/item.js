a15e842acabc1e3e3ded99e044e818fdcde6cacf
REFH4CK <refh4ck.dev@gmail.com>
2025-10-25 19:04:06 -0400

    feat: add new season item

--- frontend/src/components/utils/item.js
+++ frontend/src/components/utils/item.js
@@ -1,29 +1,30 @@
-import AncientClock from "../../assets/images/ancient_clock.png";
-import BrokenPillar from "../../assets/images/broken_pillar.webp";
-import CamelBone from "../../assets/images/camel_bone.webp";
-import ClamShell from "../../assets/images/clam_shell.webp";
-import CowSkull from "../../assets/images/cow_skull.png";
-import Crab from "../../assets/images/crab.png";
-import Pipi from "../../assets/images/pipi.webp";
-import Sand from "../../assets/images/sand.webp";
-import PirateBounty from "../../assets/images/pirate_bounty.webp";
-import Scarab from "../../assets/images/scarab.webp";
-import SeaCucumber from "../../assets/images/sea_cucumber.png";
-import SeaWeed from "../../assets/images/seaweed.webp";
-import StarFish from "../../assets/images/starfish.png";
-import Vase from "../../assets/images/vase.webp";
-import WoodenCompass from "../../assets/images/wooden_compass.webp";
-import Coral from "../../assets/images/coral.png";
-import CockleShell from "../../assets/images/cockle_shell.webp";
-import Hieroglyph from "../../assets/images/hieroglyph.webp";
-import OldBottle from "../../assets/images/old_bottle.png";
-import Pearl from "../../assets/images/pearl.webp";
-import IronCompass from "../../assets/images/iron_compass.webp";
-import EmeraldCompass from "../../assets/images/emerald_compass.webp";
-import Nothing from "../../assets/images/nothing.png";
-import Possible from "../../assets/images/possible.png";
-import Wood from "../../assets/images/wood.png";
-import Stone from "../../assets/images/stone.png";
+import AncientClock from "@/assets/images/ancient_clock.png";
+import BrokenPillar from "@/assets/images/broken_pillar.webp";
+import CamelBone from "@/assets/images/camel_bone.webp";
+import ClamShell from "@/assets/images/clam_shell.webp";
+import CowSkull from "@/assets/images/cow_skull.png";
+import Crab from "@/assets/images/crab.png";
+import Pipi from "@/assets/images/pipi.webp";
+import Sand from "@/assets/images/sand.webp";
+import PirateBounty from "@/assets/images/pirate_bounty.webp";
+import Scarab from "@/assets/images/scarab.webp";
+import SeaCucumber from "@/assets/images/sea_cucumber.png";
+import SeaWeed from "@/assets/images/seaweed.webp";
+import StarFish from "@/assets/images/starfish.png";
+import Vase from "@/assets/images/vase.webp";
+import WoodenCompass from "@/assets/images/wooden_compass.webp";
+import Coral from "@/assets/images/coral.png";
+import CockleShell from "@/assets/images/cockle_shell.webp";
+import Hieroglyph from "@/assets/images/hieroglyph.webp";
+import OldBottle from "@/assets/images/old_bottle.png";
+import Pearl from "@/assets/images/pearl.webp";
+import IronCompass from "@/assets/images/iron_compass.webp";
+import EmeraldCompass from "@/assets/images/emerald_compass.webp";
+import Nothing from "@/assets/images/nothing.png";
+import Possible from "@/assets/images/possible.png";
+import Wood from "@/assets/images/wood.png";
+import Stone from "@/assets/images/stone.png";
+import MoonCrystal from "@/assets/images/moon_crystal.webp";
 
 export const digItems = {
   Possible: Possible,
@@ -35,6 +36,7 @@ export const digItems = {
   "Clam Shell": ClamShell,
   "Old Bottle": OldBottle,
   Vase: Vase,
+  "Moon Crystal": MoonCrystal,
   Hieroglyph: Hieroglyph,
   "Ancient Clock": AncientClock,
   "Sea Cucumber": SeaCucumber,
@@ -48,9 +50,4 @@ export const digItems = {
   "Wooden Compass": WoodenCompass,
   "Cockle Shell": CockleShell,
   "Pirate Bounty": PirateBounty,
-  Scarab: Scarab,
-  "Cow Skull": CowSkull,
-  "Iron Compass": IronCompass,
-  "Emerald Compass": EmeraldCompass,
-  "Broken Pillar": BrokenPillar,
 };
