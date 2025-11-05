4f23fea952f88245bfb09eaaa3f81dac4bf02934
REFH4CK <refh4ck.dev@gmail.com>
2025-11-04 23:46:06 -0400

    feat: add patterns route with :id

--- backend/src/routes/sflRoutes.js
+++ backend/src/routes/sflRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import sflDiggingData from '../controllers/sflData.js';
+import sflPatterns from '../controllers/sflPatterns.js';
 
 const router = Router();
 
 router.get('/:id', sflDiggingData.digging);
+router.get('/patterns/:id', sflPatterns.patterns);
 
 export default router;
