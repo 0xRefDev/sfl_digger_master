0d71d109ca5ed2d4ea09a4093a3a47ee0655323c
REFH4CK <refh4ck.dev@gmail.com>
2025-11-06 17:58:28 -0400

    feat: new dig master header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,39 +1,13 @@
+import { GoBack } from "@/components/GoBack";
 import logo from "@/assets/images/logo.png";
-import discord from "@/assets/images/discord.png";
 
 export function Header() {
   return (
     <>
-      <header className="relative flex items-center justify-center flex-col p-4 pb-0 gap-3 max-w-[550px] mx-auto">
-        <a
-          href="https://discord.gg/MexBzYbxju"
-          noreferrer="true"
-          target="_blank"
-          className="absolute top-4 left-4"
-        >
-          <img
-            src={discord}
-            alt="Discord logo pixel art"
-            className="size-[3.25rem] drop-shadow-amber-100 drop-shadow-sm"
-            style={{ WebkitUserDrag: "none" }}
-          />
-        </a>
-        <button
-          className="absolute top-4 right-4 bg-white/20 rounded-lg border-2 border-lime-100 p-2 transition-colors duration-300 cursor-pointer"
-          onClick={() =>
-            window.open(
-              "https://docs.google.com/spreadsheets/d/1mtb3budL7RBDsV7-ly3C9RLyWtgr0Bbmk5DNVqKX5Dg/edit?gid=0#gid=0",
-              "_blank"
-            )
-          }
-        >
-          <img
-            src="/excel-icon.png"
-            alt="Excel list"
-            className="w-[2.25rem] h-auto"
-            style={{ WebkitUserDrag: "none" }}
-          />
-        </button>
+      <header className="flex items-center justify-center w-fit relative flex-col p-4 pb-0 gap-3 max-w-[550px] mx-auto">
+        <span className="absolute -left-14 top-0 block size-20">
+          <GoBack />
+        </span>
         <img
           src={logo}
           alt="Dig Master Logo - Pixel ART"
