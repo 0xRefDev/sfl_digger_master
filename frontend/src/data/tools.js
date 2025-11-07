94f5cfa4774cc76bf936473a3b22331566f4cb96
REFH4CK <refh4ck.dev@gmail.com>
2025-11-07 13:23:52 -0400

    feat: add description to both tools

--- frontend/src/data/tools.js
+++ frontend/src/data/tools.js
@@ -1,4 +1,14 @@
 export const tools = [
-  { name: "Dig Master", sprite: "digging", url: "/dig_master" },
-  { name: "Pumpkin Quest", sprite: "", url: "/quests" },
+  {
+    name: "Dig Master",
+    sprite: "digging",
+    url: "/dig_master",
+    description: " A tool that will help you with your daily excavations, easy to use and intuitive.",
+  },
+  {
+    name: "Pumpkin Quest",
+    sprite: "",
+    url: "/quests",
+    description: "A simple tool to view responses from Telegram bot missions and optimize rewards.",
+  },
 ];
